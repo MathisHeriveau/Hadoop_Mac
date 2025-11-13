e73914c596197683f441228d2159f2e7cef0ee5d
MathisHeriveau <mistigri.heriveau@gmail.com>
2025-11-13 15:17:22 +0100

    Automatisation

--- wordcount-enseignant/WCDriver.java
+++ wordcount-enseignant/WCDriver.java
@@ -7,6 +7,8 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.util.Tool;
 
 public class WCDriver extends Configured implements Tool {
 
